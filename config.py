c5bada0b4b7a094e94df0592c72f3e13ff43ae86
Csyyy <90692873+ChuaShiYing@users.noreply.github.com>
2025-12-10 17:01:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ch9koxkzd6rg.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bmit3273"
+customdb = "employee"
+custombucket = "chuashiying-employee"
+customregion = "us-east-1"
+
